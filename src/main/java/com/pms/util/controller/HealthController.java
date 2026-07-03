cf48af8bc7549bc1156e458a60d3e7521abf4a15
TwinkleKadia123 <kadia.twinkle.kict@gmail.com>
2026-07-03 10:37:54 +0530

    PMS - health check hit on render.com

---
+++ src/main/java/com/pms/util/controller/HealthController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.pms.util.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ */
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
